254626d6e991d03cea0f2d016a3d624dedfd9018
Student <student@RU-017.local>
2015-09-22 16:14:05 -0600

    First Commit

---
+++ main.js
@@ -0,0 +1,24 @@
+angular.module ('may app', [])
+
+var mainControllerFunc=function($scope){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+angular.module('myApp').controller('mainController' , [ '$scope', mainControllerFunc])
